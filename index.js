3704fa6f1e3c4213e7f44d6354755bce4ed244a7
Mr Whipp <dwhipp88@gmail.com>
2022-04-13 01:15:33 -0600

    page overview logic

--- index.js
+++ index.js
@@ -36,7 +36,33 @@ const url = require("url");
 // console.log("Will read file!");
 
 // *** Server ***
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const templateOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(
   `${__dirname}/starter/dev-data/data.json`,
   "utf-8"
@@ -46,14 +72,27 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("Welcome to the overview");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join("");
+    // console.log(cardsHtml);
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
   } else if (pathName === "/product") {
+    // Product page
     res.end("This is the product");
   } else if (pathName === "/api") {
+    // API page
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   } else {
+    // Not Found
     res.writeHead(404, {
       "Content-type": "text/html",
       "custom-header": "hello-world",
